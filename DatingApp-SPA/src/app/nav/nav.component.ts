91b304062e85a12ac5b7594d6e5d23ca3b76b30d
Ravinder Reddy K <k.ravinderreddy@gmail.com>
2019-09-25 01:56:45 +0530

    Routing Module Changes.

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class NavComponent implements OnInit {
    model: any = {} ;
-   constructor(public authService: AuthService, private alertify: AlertifyService) { }
+   constructor(public authService: AuthService, private alertify: AlertifyService, private route: Router) { }
 
   ngOnInit() {
   }
@@ -23,6 +24,9 @@ export class NavComponent implements OnInit {
       error => {
         // console.log(error);
         this.alertify.error(error.message);
+      }, 
+      () => {
+        this.route.navigate(['/members']);
       });
   }
 
@@ -34,6 +38,7 @@ export class NavComponent implements OnInit {
     localStorage.removeItem('token');
     // console.log('logged out');
     this.alertify.message('logged out');
+    this.route.navigate(['/home']);
   }
 
 }
